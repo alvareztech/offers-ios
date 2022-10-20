81f4fabdd337d99323565a85b49505239e3d3f74
Daniel Alvarez <daniel@alvarez.tech>
2022-10-20 10:57:00 -0700

    Initial Commit

---
+++ Offers/OffersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffersApp.swift
+//  Offers
+//
+//  Created by Daniel Alvarez on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct OffersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
